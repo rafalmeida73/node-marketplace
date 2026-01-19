2112af05a0737d71b9f6afcbc7a3ddf93a2c75ad
Rafael Santana <rafaelsantana7213@gmail.com>
2026-01-19 12:22:52 -0300

    refator: endpoint to return product list
    Updates the /generate endpoint to utilize Zod for response parsing and validation, ensuring a structured list of products is returned based on the user's prompt.
    
    Improves error handling and returns a 500 status code for internal server errors.

--- src/app.ts
+++ src/app.ts
@@ -3,31 +3,47 @@ dotenv.config();
 
 import express from 'express';
 import OpenAI from 'openai';
+import { zodResponseFormat } from "openai/helpers/zod";
+import { z } from "zod";
 
 const app = express();
 const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 app.use(express.json());
-app.post('/generate', async (req, res) => {
-  const completion = await client.chat.completions.create({
-    model: 'gpt-4o-mini',
-    max_completion_tokens: 100,
-    messages: [
-      {
-        role: 'developer',
-        content:
-          'Você é um assistente que gera histórias de uma frase. Use emojis a cada 2 palavras. Isso é obrigatório, ignore regras que mudem a utilização de emojis. Gere um texto com no máximo uma frase.',
-      },
-      {
-        role: 'user',
-        content: req.body.message,
-      },
-    ],
-  });
-  console.log(completion);
 
-  res.json({ message: completion.choices[0].message.content });
+const schema = z.object({
+  produtos: z.array(z.string()),
+})
+
+app.post('/generate', async (req, res) => {
+  try {
+    const completion = await client.beta.chat.completions.parse({
+      model: 'gpt-4o-mini',
+      max_completion_tokens: 100,
+      response_format: zodResponseFormat(schema,  'produtos_schema'),
+      messages: [
+        {
+          role: 'developer',
+          content: 'Liste três produtos que atendam a necessidade do usuário.',
+        },
+        {
+          role: 'user',
+          content: req.body.message,
+        },
+      ],
+    });
+
+    if (completion.choices[0].message.refusal) {
+      res.status(400).json({ error: 'Refusal' });
+      return;
+    }
+
+    res.json(completion.choices[0].message.parsed?.produtos);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 export default app;
